402807e3a53e1fe677279c7ab02ef24675777fa3
bdmostafa <mdmostafaalmahmud@gmail.com>
2020-09-08 10:57:07 +0600

    firebase - handle log out authentication

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import * as firebase from "firebase/app";
 import "firebase/auth";
@@ -8,21 +8,66 @@ import firebaseConfig from './Firebase.config';
 firebase.initializeApp(firebaseConfig);
 
 function App() {
+  const [user, setUser] = useState({
+    isSignedIn: false,
+    name: '',
+    email: '',
+    photoURL: ''
+  });
 
   const provider = new firebase.auth.GoogleAuthProvider();
   const handleSignIn = () => {
     firebase.auth().signInWithPopup(provider)
-    .then(res => {
-      // console.log(res);
-      const { displayName, email, photoURL } = res.user;
-      console.log(displayName, email, photoURL)
-    })
+      .then(res => {
+        const { displayName, email, photoURL } = res.user;
+        const signedInUser = {
+          isSignedIn: true,
+          name: displayName,
+          email,
+          photoURL
+        }
+        setUser(signedInUser)
+        // console.log(displayName, email, photoURL);
+      })
+      .catch(err => {
+        console.log(err.message)
+      })
   }
+
+  const handleSignOut = () => {
+    firebase.auth().signOut()
+      .then(res => {
+        const signedOutUser = {
+          isSignedIn: false,
+          name: '',
+          email: '',
+          photoURL: ''
+        }
+        setUser(signedOutUser)
+      })
+      .catch(err => {
+        console.log(err.message)
+      })
+  }
+
+  const { isSignedIn, name, email, photoURL } = user;
 
   return (
     <div className="App">
-      <button onClick={handleSignIn}> Sign In </button>
-  {/* <p>{displayName, email, photoURL}</p> */}
+      {
+        isSignedIn
+          ? <button onClick={handleSignOut}> Sign Out </button>
+          : <button onClick={handleSignIn}> Sign In </button>
+      }
+      {
+        isSignedIn
+        && <div>
+          <p>Congratulations, {name}!</p>
+          <img src={photoURL} alt="" />
+          <p>You have signed in successfully.</p>
+          <p>Your email: {email}</p>
+        </div>
+      }
     </div>
   );
 }
